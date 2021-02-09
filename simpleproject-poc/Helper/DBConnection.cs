c01740a2c2c5e7112b591262c744096ce95985bc
Fabrice Reiser Notebook <reiser.fabrice@gmail.com>
2021-02-09 10:49:00 +0100

    Added MVVM structure, added DBConnection helper, added first model

---
+++ simpleproject-poc/Helper/DBConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+
+namespace simpleproject_poc.Helper
+{
+    class DBConnection
+    {
+        public DBConnection()
+        {
+            SetupDBConnection();
+        }
+
+        public DataContext SetupDBConnection()
+        {
+            DataContext dbConnection = new DataContext("Server=DESKTOP-35P8P5I\\SQLEXPRESS;Database=Librarysystem;Connection timeout=30;Integrated Security=True");
+            return dbConnection;
+        }
+    }
+}
---
+++ simpleproject-poc/Models/ProjectMethod.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using simpleproject_poc.Helper;
+
+namespace simpleproject_poc.Models
+{
+    class ProjectMethod
+    {
+        public int PK { get; }
+        public string ProjectMethodName { get; }
+
+        public ProjectMethod(int t_PK, string t_ProjectMethodName)
+        {
+            PK = t_PK;
+            ProjectMethodName = t_ProjectMethodName;
+        }
+
+        public void Create()
+        {
+            DBConnection dbConnObj = new DBConnection();
+            
+        }
+
+    }
+
+    class dbProjectMethod
+    {
+        //SQL mapping
+        [Table(Name = "Project_method")]
+        public class mappingProjectMethod
+        {
+            //Mapper auf Primary Key
+            [Column(Name = "PKey", IsDbGenerated = true, IsPrimaryKey = true)]
+            public int Pkey_1
+            {
+                get;
+                set;
+            }
+
+            //Mapper auf Feld Name der Gruppe
+            [Column]
+            public string Project_method_name;
+        }
+    }
+}
