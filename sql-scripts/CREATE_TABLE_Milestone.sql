f21e7a5119ef35dee347137f971450e24da93cf3
Fabrice Reiser <reiser.fabrice@gmail.com>
2021-02-16 13:57:25 +0100

    Changed relationship between Project_phase and Milestone

--- sql-scripts/CREATE_TABLE_Milestone.sql
+++ sql-scripts/CREATE_TABLE_Milestone.sql
@@ -1,7 +1,7 @@
 USE [simpleproject-poc]
 GO
 
-/****** Object:  Table [dbo].[Milestone]    Script Date: 06.02.2021 17:03:34 ******/
+/****** Object:  Table [dbo].[Milestone]    Script Date: 16.02.2021 13:56:29 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -12,10 +12,18 @@ CREATE TABLE [dbo].[Milestone](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[Milestone_name] [varchar](50) NOT NULL,
 	[Date] [date] NOT NULL,
+	[Project_phase_id] [int] NOT NULL,
  CONSTRAINT [PK_Milestone] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Milestone]  WITH CHECK ADD  CONSTRAINT [FK_Milestone_Project_phase] FOREIGN KEY([Project_phase_id])
+REFERENCES [dbo].[Project_phase] ([Id])
+GO
+
+ALTER TABLE [dbo].[Milestone] CHECK CONSTRAINT [FK_Milestone_Project_phase]
 GO
 
--- sql-scripts/CREATE_TABLE_Project_phase.sql
+++ sql-scripts/CREATE_TABLE_Project_phase.sql
@@ -1,7 +1,7 @@
 USE [simpleproject-poc]
 GO
 
-/****** Object:  Table [dbo].[Project_phase]    Script Date: 06.02.2021 17:02:41 ******/
+/****** Object:  Table [dbo].[Project_phase]    Script Date: 16.02.2021 13:55:58 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -23,19 +23,11 @@ CREATE TABLE [dbo].[Project_phase](
 	[Phase_documents_link] [varchar](250) NULL,
 	[Project_id] [int] NOT NULL,
 	[Phase_id] [int] NOT NULL,
-	[Milestone_id] [int] NOT NULL,
  CONSTRAINT [PK_Project_phase] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[Project_phase]  WITH CHECK ADD  CONSTRAINT [FK_Project_phase_Milestone] FOREIGN KEY([Milestone_id])
-REFERENCES [dbo].[Milestone] ([Id])
-GO
-
-ALTER TABLE [dbo].[Project_phase] CHECK CONSTRAINT [FK_Project_phase_Milestone]
 GO
 
 ALTER TABLE [dbo].[Project_phase]  WITH CHECK ADD  CONSTRAINT [FK_Project_phase_Phase] FOREIGN KEY([Phase_id])
