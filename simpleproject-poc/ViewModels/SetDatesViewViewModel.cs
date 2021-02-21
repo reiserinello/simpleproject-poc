d23192ff3f9f28537a6a3dfb24dbe7dad402d8be
Fabrice Reiser <reiser.fabrice@gmail.com>
2021-02-21 21:19:59 +0100

    Started SetDates logic

---
+++ simpleproject-poc/ViewModels/SetDatesViewViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Prism.Commands;
+using simpleproject_poc.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace simpleproject_poc.ViewModels
+{
+    class SetDatesViewViewModel : MainViewModel
+    {
+        public Nullable<DateTime> _datepickStartDate;
+        public Nullable<DateTime> _datepickEndDate;
+
+        public Nullable<DateTime> datepickStartDate
+        {
+            get
+            {
+                return _datepickStartDate;
+            }
+            set
+            {
+                _datepickStartDate = value;
+                OnPropertyChanged("datepickStartDate");
+            }
+        }
+
+        public Nullable<DateTime> datepickEndDate
+        {
+            get
+            {
+                return _datepickEndDate;
+            }
+            set
+            {
+                _datepickEndDate = value;
+                OnPropertyChanged("datepickEndDate");
+            }
+        }
+
+        // Button Projekt freigeben
+        public ICommand btnSetDates
+        {
+            get { return new DelegateCommand<object>(SetDates); }
+        }
+
+        private void SetDates(object context)
+        {
+            DBUpdate dbUpdateObj = new DBUpdate();
+            dbUpdateObj.SetProjectApprovalDate(this);
+        }
+    }
+}
